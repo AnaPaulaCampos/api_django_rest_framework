cd9af91e17b75f21fb9eca491d0cf3f22d1ce784
AnaPaulaCampos <paulinha.vhh@gmail.com>
2022-08-04 07:18:55 -0700

    validar estado com try

--- api/api/atualizarpedidosview.py
+++ api/api/atualizarpedidosview.py
@@ -1,6 +1,7 @@
 
 from http.client import responses
 import json
+from api.api.pedidoservices import PedidoServices
 from api.api.serializers import AtualizarPedidoSerializer
 from api.models import Pedido
 from rest_framework.response import Response
@@ -10,25 +11,50 @@ from rest_framework.views import APIView
 
 class AtualizarPedidoAPIView(APIView):
     serializer_class = AtualizarPedidoSerializer
+
     # throttle_scope = "cars_app"
 
    
 
+    # def put(self, request, *args, **kwargs):
+
+    #     # idPedido = request.query_params.get('id')
+    #     # idPedido = request.query_params["ID"]
+
+    #     data = request.data
+    #     pedido = Pedido.objects.get(ID=1)
+
+    #     novoEstado = data["estadoAtualPedido"]
+
+    #     podeAtualizarEstado = PedidoServices.validarEstado(pedido.estadoAtualPedido, novoEstado)
+
+    #     if podeAtualizarEstado:
+    #         pedido.estadoAtualPedido = novoEstado
+        
+    #     pedido.save()
+    #     serializer = AtualizarPedidoSerializer(pedido)
+    #     return Response(serializer.data)
+
     def put(self, request, *args, **kwargs):
 
-        idPedido = request.query_params.get('id')
-        # idPedido = request.query_params["ID"]
-        pedido = Pedido.objects.get(ID=1)
 
         data = request.data
+        pedido = Pedido.objects.get(ID=1)
+        novoEstado = data["estadoAtualPedido"]
+        podeAtualizarEstado = PedidoServices.validarEstado(pedido.estadoAtualPedido, novoEstado)
 
-        pedido.estadoAtualPedido = data["estadoAtualPedido"]
+        try:
+            if podeAtualizarEstado:
+                pedido.estadoAtualPedido = novoEstado
+                pedido.save()
+                serializer = AtualizarPedidoSerializer(pedido)
+            return Response(serializer.data)
+        except podeAtualizarEstado:
+            serializer = AtualizarPedidoSerializer(pedido)
+            return Response({"message": "Deal doesnt exist"})
 
 
-        pedido.save()
 
-        serializer = AtualizarPedidoSerializer(pedido)
-        return Response(serializer.data)
 
 
 
---
+++ api/api/pedidoservices.py
@@ -0,0 +1,7 @@
+
+class PedidoServices():
+
+    def validarEstado(estadoAtual, novoEstado):
+            if estadoAtual != novoEstado:
+                return True
+            return False
