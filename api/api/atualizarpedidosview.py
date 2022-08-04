967ae762d33d929bacd531c1ef58a86019f58915
AnaPaulaCampos <paulinha.vhh@gmail.com>
2022-08-04 07:41:13 -0700

    aplicado try cacth para validar estado

--- api/api/atualizarpedidosview.py
+++ api/api/atualizarpedidosview.py
@@ -6,52 +6,27 @@ from api.api.serializers import AtualizarPedidoSerializer
 from api.models import Pedido
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+
 
 
 
 class AtualizarPedidoAPIView(APIView):
     serializer_class = AtualizarPedidoSerializer
 
-    # throttle_scope = "cars_app"
-
-   
-
-    # def put(self, request, *args, **kwargs):
-
-    #     # idPedido = request.query_params.get('id')
-    #     # idPedido = request.query_params["ID"]
-
-    #     data = request.data
-    #     pedido = Pedido.objects.get(ID=1)
-
-    #     novoEstado = data["estadoAtualPedido"]
-
-    #     podeAtualizarEstado = PedidoServices.validarEstado(pedido.estadoAtualPedido, novoEstado)
-
-    #     if podeAtualizarEstado:
-    #         pedido.estadoAtualPedido = novoEstado
-        
-    #     pedido.save()
-    #     serializer = AtualizarPedidoSerializer(pedido)
-    #     return Response(serializer.data)
-
     def put(self, request, *args, **kwargs):
-
-
         data = request.data
         pedido = Pedido.objects.get(ID=1)
         novoEstado = data["estadoAtualPedido"]
         podeAtualizarEstado = PedidoServices.validarEstado(pedido.estadoAtualPedido, novoEstado)
-
         try:
             if podeAtualizarEstado:
                 pedido.estadoAtualPedido = novoEstado
                 pedido.save()
                 serializer = AtualizarPedidoSerializer(pedido)
             return Response(serializer.data)
-        except podeAtualizarEstado:
-            serializer = AtualizarPedidoSerializer(pedido)
-            return Response({"message": "Deal doesnt exist"})
+        except:  
+            return Response({"message": "Estado nao permitido !", "Pedido": pedido.estadoAtualPedido},status = status.HTTP_400_BAD_REQUEST )
 
 
 
